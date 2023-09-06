fbb41613df21284e434b1607cd7b4c5f410df9f9
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-09-06 14:46:31 +0300

    removed folder schemas

---
+++ controllers/auths.js
@@ -0,0 +1,44 @@
+const bcrypt = require("bcrypt");
+
+const { User } = require("../models/user");
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email already used");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUsser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    email: newUsser.email,
+    name: newUsser.name,
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
+    
+  const token = "gatyq53yghateh54q";
+
+  res.json({ token });
+};
+
+module.exports = {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+};
--- models/contact.js
+++ models/contact.js
@@ -3,22 +3,25 @@ const Joi = require("joi");
 
 const { handleMongooseError } = require("../helpers");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 contactSchema.post("save", handleMongooseError);
 
@@ -49,3 +52,4 @@ const schemas = {
 const Contact = model("contact", contactSchema);
 
 module.exports = { Contact, schemas };
+
