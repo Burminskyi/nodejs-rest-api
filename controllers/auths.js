5b33a2495f7150c3a7b606d044459f0149254be5
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-09-11 18:15:48 +0300

    added picture resizing

--- controllers/auths.js
+++ controllers/auths.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User } = require("../models/user");
 
@@ -71,10 +72,20 @@ const logout = async (req, res) => {
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
+
+  try {
+    const image = await Jimp.read(tempUpload);
+    image.resize(250, 250).write(tempUpload);
+  } catch (error) {
+    console.log("error: ", error);
+  }
+
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
+
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
+
   await User.findByIdAndUpdate(_id, { avatarURL });
 
   res.json({
